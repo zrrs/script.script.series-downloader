baae271ab1ef023690e6f2cec3e57370878e0f42
zrrs <zurrias@gmail.com>
2015-10-13 22:51:49 +0200

    Directory structure created.

---
+++ main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*- 
+#-------------------------------------------------------------------------------
+# Name:        Series Downloader
+# 
+# Author:      zrrs
+#
+# Created:     13/10/2015
+#
+# ---------------------------------------
+# Built-in modules, uncomment if you need.
+#
+# import xbmc
+# import xbmcgui
+# import xbmcplugin
+# import xbmcaddon
+# import xbmcvfs
+
